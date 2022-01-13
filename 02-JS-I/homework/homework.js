bb67ba70882d70006f40f5815a8e9b625b8ff36e
jose <joseandrescolmenares02@gmail.com>
2022-01-13 11:34:03 -0300

    tarea lista

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -245,8 +245,11 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
  
+  if (letra.length >1){
+    return "Dato incorrecto"
+  }
     
-  if (letra=='a'|| letra=='e' || letra=='i' || letra=='o' || letra=='u'){
+ else if (letra=='a'|| letra=='e' || letra=='i' || letra=='o' || letra=='u'){
     return "Es vocal"
   }else{
     return "Dato incorrecto"
